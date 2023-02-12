8231ecdd9e321555f03176cac2455028be40adda
parag <paragjain528@gmail.com>
2023-02-12 23:01:15 +0530

    Storing the new movies into the Database

--- src/App.js
+++ src/App.js
@@ -11,46 +11,56 @@ function App() {
 
   console.log('app rendered')
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     console.log('inside fetchMovieHandler function ')
     setIsLoading(true);
-    console.log(isLoading)
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-http-bb271-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
-
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      console.log('line 34')
-      setMovies(transformedMovies);
-      console.log('line 36')
+
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
       const cleanInterval = setInterval(async () => {
-        await fetch("https://swapi.dev/api/films/");
+        await fetch("https://react-http-bb271-default-rtdb.firebaseio.com/movies.json");
       }, 5000);
       setStop(cleanInterval);
     }
     setIsLoading(false);
-  };
+  },[]);
  
   useEffect(() => {
     console.log('usEffect called')
     fetchMoviesHandler()
   }, []);
 
-  function addMovieHandler(movie) {
+ async function addMovieHandler(movie) {
     console.log(movie);
+    const response = await fetch(
+      "https://react-http-bb271-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   const stopFetching = () => {
